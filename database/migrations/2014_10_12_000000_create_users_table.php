17e148dbf1f2ada28b970e09ace539043ac522f9
Andres G. Carmona <ing.andres.carmona@gmail.com>
2020-09-23 17:23:14 -0500

    Created users migration.

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,36 +1,45 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    class CreateUsersTable extends Migration
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
-        });
-    }
+        /**
+         * Run the migrations.
+         *
+         * @return void
+         */
+        public function up(): void
+        {
+            Schema::create('users', function (Blueprint $table)
+            {
+                $table->id();
+                $table->string('display_name')->nullable();
+                $table->string('username')->nullable();
+                $table->string('email')->nullable()->unique();
+                $table->string('password');
+                $table->string('avatar')->nullable();
+                $table->string('country')->nullable();
+                $table->string('href')->nullable();
+                $table->string('uri')->nullable();
+                $table->string('access_token');
+                $table->string('refresh_token');
+                $table->dateTime('token_expires_at');
+                $table->rememberToken();
+                $table->timestamps();
+                $table->softDeletes();
+            });
+        }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('users');
+        /**
+         * Reverse the migrations.
+         *
+         * @return void
+         */
+        public function down(): void
+        {
+            Schema::dropIfExists('users');
+        }
     }
-}
