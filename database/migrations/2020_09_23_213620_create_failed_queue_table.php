1b41fd2b757a2b9a6fe6a85a092022a127201256
Andres G. Carmona <ing.andres.carmona@gmail.com>
2020-09-23 17:23:58 -0500

    Updated queue config changing the default table names. Created migrations from queue and failed_queue.

--- database/migrations/2020_09_23_213620_create_failed_queue_table.php
+++ database/migrations/2020_09_23_213620_create_failed_queue_table.php
@@ -4,16 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFailedJobsTable extends Migration
+class CreateFailedQueueTable extends Migration
 {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
+    public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('failed_queue', function (Blueprint $table) {
             $table->id();
             $table->string('uuid')->unique();
             $table->text('connection');
@@ -29,8 +29,8 @@ class CreateFailedJobsTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('failed_queue');
     }
 }
