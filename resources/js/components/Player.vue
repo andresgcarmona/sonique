265446c81da39330e0ba141ce784d451e63998a7
Andres G. Carmona <ing.andres.carmona@gmail.com>
2020-11-10 10:07:48 -0600

    Added interval to update time in track.

--- resources/js/components/Player.vue
+++ resources/js/components/Player.vue
@@ -1,28 +1,46 @@
 <template>
-  <div class="player-container fixed bottom-0 z-50 p-4 w-full flex">
-    <div class="song-metadata flex flex-1 items-center">
-      <template v-if="currentTrack">
-        <img :src="cover" alt="Album art" class="h-16 w-16 block mr-3"/>
-        <div class="flex flex-col">
-          <span>{{ artists }}</span>
-          <span>{{ trackName }}</span>
-        </div>
-      </template>
+  <div class="player-container fixed bottom-0 z-50 w-full flex flex-col">
+    <div class="progress progress-xs">
+      <div class="progress-bar" role="progressbar"
+           aria-valuenow="75"
+           aria-valuemin="0"
+           aria-valuemax="100"
+           :style="{width: trackProgress + '%'}"></div>
     </div>
-    <div class="flex flex-grow">
-      <div class="media-controls flex items-center">
-        <div class="media-button">
-          <button class="btn btn-sm" @click="play(currentTrack)"><i class="fas fa-fw fa-step-backward fa-lg"></i>
-          </button>
+    
+    <div class="flex p-4">
+      <div class="song-metadata flex flex-1 items-center">
+        <template v-if="currentTrack">
+          <img :src="cover" alt="Album art" class="h-16 w-16 block mr-3"/>
+          <div class="flex flex-col">
+            <span>{{ artists }}</span>
+            <span>{{ trackName }}</span>
+          </div>
+        </template>
+      </div>
+      <div class="flex flex-grow">
+        <div class="media-controls flex items-center">
+          <div class="media-button">
+            <button class="btn btn-sm" @click="play(currentTrack)"><i class="fas fa-fw fa-step-backward fa-lg"></i>
+            </button>
+          </div>
+          <div class="media-button mx-2">
+            <button class="btn" v-if="!isPlaying" @click="play(currentTrack)"><i class="fas fa-fw fa-play fa-lg"></i>
+            </button>
+            <button class="btn" v-if="isPlaying" @click="pause"><i class="fas fa-fw fa-pause fa-lg"></i></button>
+          </div>
+          <div class="media-button">
+            <button class="btn btn-sm" @click="play(currentTrack)"><i class="fas fa-fw fa-step-forward fa-lg"></i>
+            </button>
+          </div>
         </div>
-        <div class="media-button mx-2">
-          <button class="btn" v-if="!isPlaying" @click="play(currentTrack)"><i class="fas fa-fw fa-play fa-lg"></i>
-          </button>
-          <button class="btn" v-if="isPlaying" @click="pause"><i class="fas fa-fw fa-pause fa-lg"></i></button>
-        </div>
-        <div class="media-button">
-          <button class="btn btn-sm" @click="play(currentTrack)"><i class="fas fa-fw fa-step-forward fa-lg"></i>
-          </button>
+      </div>
+      
+      <div class="flex-grow-0 flex items-center">
+        <div>
+          <span>{{ trackProgress }}</span>
+          <span>-</span>
+          <span>{{ trackDuration }}</span>
         </div>
       </div>
     </div>
@@ -31,6 +49,7 @@
 
 <script>
   import { mapActions, mapGetters, mapMutations } from 'vuex'
+  import { find, padStart } from 'lodash'
   
   const PLAYER_INFO_INTERVAL = 5 * 1000 // 5 seconds.
   
@@ -43,6 +62,9 @@
         currentTrack: null,
         isPlaying: false,
         getPlayerInfoInterval: null,
+        tickInterval: null,
+        trackProgress: 0,
+        progressMs: 0,
       }
     },
     methods: {
@@ -90,7 +112,10 @@
         this.player.addListener('ready', async({ deviceId }) => {
           if(!deviceId) {
             const devices = await this.getDevices()
-            deviceId      = devices[0]
+            
+            deviceId = find(devices, device => {
+              return device.name === 'Sonique player one'
+            })
           }
           
           if(deviceId) {
@@ -130,7 +155,9 @@
         this.isPlaying    = true
         
         this.getPlayerInfo()
-        this.getPlayerInfoInterval = setInterval(this.getPlayerInfo, PLAYER_INFO_INTERVAL)
+        
+        this.tickInterval          = setInterval(this.updateTrackProgress, 1000)
+        this.getPlayerInfoInterval = setInterval(this.updatePlayerInfo, PLAYER_INFO_INTERVAL)
       },
       async pause() {
         this.isPlaying = false
@@ -140,9 +167,29 @@
         })
         
         this.getPlayerInfo()
+        
         if(this.getPlayerInfoInterval) {
           clearInterval(this.getPlayerInfoInterval)
         }
+        
+        if(this.tickInterval) {
+          clearInterval(this.tickInterval)
+        }
+      },
+      async updatePlayerInfo() {
+        //this.getPlayerInfo()
+
+//        if(this.playerInfo) {
+//          this.trackProgress = Math.floor((this.playerInfo.progress_ms / this.playerInfo.item.duration_ms) * 100)
+//        }
+      },
+      updateTrackProgress() {
+        if(this.currentTrack) {
+          this.progressMs += 1000
+          this.trackProgress = Math.floor((this.progressMs / this.currentTrack.duration_ms) * 100)
+        }
+        
+        return 0
       },
     },
     computed: {
@@ -161,18 +208,37 @@
         
         if(this.currentTrack && this.currentTrack.artists.items) {
           this.currentTrack.artists.items[0].forEach(artist => artists += artist.name)
-  
+          
           return artists
         }
         
         this.currentTrack.artists.forEach(artist => artists += artist.name)
-  
+        
         return artists
       },
       cover() {
         return this.currentTrack.album.images[0].url
       },
-      
+      currentProgrees() {
+        if(this.currentTrack) {
+        }
+      },
+      trackDuration() {
+        if(this.currentTrack) {
+          const durationMs = this.currentTrack.duration_ms
+          
+          let minutes = Math.floor((durationMs / 1000 / 60))
+          let seconds = Math.floor((durationMs / 1000) % 60)
+          
+          if(minutes > 60) {
+            let hours = Math.floor(minutes / 60)
+            
+            return padStart(hours + '', 2, '00') + ' h ' + padStart(minutes + '', 2, '00') + ' min'
+          }
+          
+          return padStart(minutes + '', 2, '00') + ':' + padStart(seconds + '', 2, '00')
+        }
+      },
     },
     created() {
       window.onSpotifyWebPlaybackSDKReady = async() => {
@@ -190,7 +256,7 @@
         if(info) {
           this.currentTrack = info.item
         }
-      }
+      },
     },
   }
 </script>
